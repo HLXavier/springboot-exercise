daa22ab341a92161f675c07560a156b344f20f9d
Henrique de Lima Xavier <henrique.xavier@cwi.com.br>
2022-08-31 22:03:17 -0300

    initial commit

---
+++ src/main/java/com/pucrs/t1es2/T1Es2Application.java
@@ -0,0 +1,13 @@
+package com.pucrs.t1es2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1Es2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1Es2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pucrs/t1es2/T1Es2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucrs.t1es2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1Es2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
